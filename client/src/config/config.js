cdb6a878a7920bd7fe6729a930506c1c0e94ca2e
unknown <naruto3285@gmail.com>
2025-02-16 16:44:52 +0700

    refactor(ui, api):make constant immutable using Object.freeze

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -5,9 +5,9 @@ export const AUTH_URL = "/api/v1/auth";
 export const USERS_URL = "/api/v1/users";
 export const CONVERSATIONS_URL = "/api/v1/conversations";
 
-export const messageStatus = {
+export const messageStatus = Object.freeze({
     Sending: 'sending',
     Sent: 'sent',
     Delivered: 'delivered',
     Seen: 'seen'
-};
+});
--- server/shareVariable.js
+++ server/shareVariable.js
@@ -4,19 +4,19 @@ import { dirname } from 'path';
 const __filename = fileURLToPath(import.meta.url);
 export const __dirname = dirname(__filename);
 
-export const roleName = {
+export const roleName = Object.freeze({
     User: 'user',
     Admin: 'admin',
-}
+})
 
-export const messageType = {
+export const messageType = Object.freeze({
     Text: 'text',
     Image: 'image',
     File: 'file'
-}
+})
 
-export const messageStatus = {
+export const messageStatus = Object.freeze({
     Sent: 'sent',
     Delivered: 'delivered',
     Seen: 'seen'
-}
+})
