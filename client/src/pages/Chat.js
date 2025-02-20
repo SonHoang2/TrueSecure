63905dcad5c052df8a3ef51cbfb1c6f56dab0aec
unknown <naruto3285@gmail.com>
2025-02-20 18:39:41 +0700

    feat(ui): display avatar for seen users and fix long character spam issue

--- client/src/pages/Chat.js
+++ client/src/pages/Chat.js
@@ -297,14 +297,14 @@ const Chat = ({ userStatus }) => {
                     const messageIndex = updatedMessages.findLastIndex(
                         (msg) => msg.status === messageStatus.Sending || msg.id === messageId
                     );
-            
+
                     if (messageIndex === -1) return prevState;
                     const message = updatedMessages[messageIndex];
-            
+
                     if (status === messageStatus.Seen) {
                         message.statuses = message.statuses || [];
                         message.status = null;
-                        
+
                         const statusIndex = message.statuses.findIndex((s) => s.userId === userId);
                         if (statusIndex === -1) {
                             message.statuses.push({ userId, status });
@@ -314,10 +314,12 @@ const Chat = ({ userStatus }) => {
                     } else {
                         updatedMessages[messageIndex] = { ...message, status, id: messageId };
                     }
-            
+
                     return { ...prevState, messages: updatedMessages };
                 });
-            });            
+            });
+
+
 
             return () => {
                 socket.off("new-private-message");
@@ -466,32 +468,57 @@ const Chat = ({ userStatus }) => {
                             {chatState.messages.map((msg, index) => {
                                 const isSentByUser = msg.senderId === user.id;
                                 const isLastMessage = index === chatState.messages.length - 1;
+
+                                // Find the last seen message index
+                                const lastSeenIndex = chatState.messages.reduce((acc, curr, idx) => {
+                                    if (curr.status === messageStatus.Seen) {
+                                        return idx;
+                                    }
+                                    return acc;
+                                }, -1);
+
+                                const isLastSeenMessage = index === lastSeenIndex;
+
+                                if (isLastMessage && isSentByUser) {
+                                    console.log("msg?.status", msg);
+
+                                }
+
                                 return (
-                                    <div key={index} className={`flex w-full px-2 py-1  ${isSentByUser ? "justify-end" : "justify-start"}`}>
-                                        <div className="flex max-w-md">
-                                            {!isSentByUser && (
-                                                <div className="flex pe-2 items-end">
-                                                    <img className="size-8 rounded-full" src={`${IMAGES_URL}/${chatState.receiver?.avatar}`} alt="" />
-                                                </div>
-                                            )}
-                                            <div className="flex flex-col">
-                                                <p className={`rounded-3xl px-3 py-2 break-words max-w-full text-s 
-                                                ${isSentByUser ? "bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-white" : "bg-gray-100 text-black"}`
-                                                }>
-                                                    {msg.content}
-                                                </p>
-                                                {
-                                                    isLastMessage && isSentByUser &&
-                                                    (
-                                                        <div className="flex justify-end">
-                                                            <p className="text-xs pe-3 text-gray-600 first-letter:uppercase">
+                                    <div key={index} className="flex flex-col">
+                                        <div className={`flex w-full px-2 py-1 ${isSentByUser ? "justify-end" : "justify-start"}`}>
+                                            <div className="flex max-w-md">
+                                                {!isSentByUser && (
+                                                    <div className="flex-none pe-2 items-end">
+                                                        <img className="size-8 rounded-full" src={`${IMAGES_URL}/${chatState.receiver?.avatar}`} alt="" />
+                                                    </div>
+                                                )}
+                                                <div className="flex-grow flex flex-col">
+                                                    <p className={`rounded-3xl px-3 py-2 break-all text-s 
+                                                        ${isSentByUser ? "bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-white" : "bg-gray-100 text-black"}`}>
+                                                        {msg.content}
+                                                    </p>
+                                                    <div className="flex justify-end">
+                                                        {
+                                                            isLastMessage && isSentByUser && msg?.status !== messageStatus.Seen &&
+                                                            <p className="text-xs pe-4 text-gray-600 first-letter:uppercase">
                                                                 {msg?.status}
                                                             </p>
-                                                        </div>
-                                                    )
-                                                }
+                                                        }
+                                                    </div>
+                                                </div>
                                             </div>
                                         </div>
+                                        {
+                                            isLastSeenMessage && msg?.status === messageStatus.Seen && (
+                                                <div className="flex justify-end w-full">
+                                                    <div className="flex pe-4 items-end">
+                                                        <img className="size-4 rounded-full" src={`${IMAGES_URL}/${chatState.receiver?.avatar}`} alt="" />
+                                                    </div>
+                                                </div>
+                                            )
+                                        }
+
                                     </div>
                                 )
                             })}
