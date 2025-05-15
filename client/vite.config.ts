b19914b64b995aa147aa2ad775d5f90c46cbd32b
sonhoang2 <naruto3285@gmail.com>
2025-05-15 11:25:56 +0700

    format(config)

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,30 +1,30 @@
 // vite.config.ts
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import fs from "fs";
-import path from "path";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import fs from 'fs';
+import path from 'path';
 
 export default defineConfig(() => {
-  const useHttps = process.env.HTTPS;
-  const certPath = process.env.SSL_CRT_FILE;
-  const keyPath = process.env.SSL_KEY_FILE;
+    const useHttps = process.env.HTTPS;
+    const certPath = process.env.SSL_CRT_FILE;
+    const keyPath = process.env.SSL_KEY_FILE;
 
-  console.log(`Using HTTPS: ${process.env.HTTPS}`);
+    console.log(`Using HTTPS: ${process.env.HTTPS}`);
 
-  const serverConfig = useHttps
-    ? {
-        https: {
-          cert: fs.readFileSync(path.resolve(certPath)),
-          key: fs.readFileSync(path.resolve(keyPath)),
+    const serverConfig = useHttps
+        ? {
+              https: {
+                  cert: fs.readFileSync(path.resolve(certPath)),
+                  key: fs.readFileSync(path.resolve(keyPath)),
+              },
+          }
+        : {};
+
+    return {
+        plugins: [react()],
+        server: {
+            port: 3000,
+            ...serverConfig, // Spread the HTTPS configuration only if needed
         },
-      }
-    : {};
-
-  return {
-    plugins: [react()],
-    server: {
-      port: 3000,
-      ...serverConfig, // Spread the HTTPS configuration only if needed
-    },
-  };
+    };
 });
