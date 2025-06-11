f12b367358a1582aebdb843d5387b6658b4ba024
sonhoang2 <naruto3285@gmail.com>
2025-06-11 10:35:00 +0700

    feat(DeepfakeDetection): enhance CUDA availability checks and improve inference messages
    fix(Main): add uvicorn run command for starting the FastAPI application
    
    chore(Requirements): reorder dependencies for better clarity

--- df_server/deepfake_detection.py
+++ df_server/deepfake_detection.py
@@ -5,13 +5,19 @@ from pytorch_grad_cam import GradCAM
 from pytorch_grad_cam.utils.model_targets import ClassifierOutputTarget
 from pytorch_grad_cam.utils.image import show_cam_on_image
 from PIL import ImageFont, ImageDraw, Image
-import numpy as np 
+import numpy as np
 import cv2
 
 from face_detection import detect_bounding_box
 
-print("torch.cuda.is_available()" , torch.cuda.is_available())  # Should return True if CUDA is available
-print("torch.cuda.get_device_name(0)", torch.cuda.get_device_name(0))  # Should return the name of your GPU 
+# Should return True if CUDA is available
+print("torch.cuda.is_available()", torch.cuda.is_available())
+
+if torch.cuda.is_available():
+    print("torch.cuda.get_device_name(0):", torch.cuda.get_device_name(0))
+    print("Using GPU for inference")
+else:
+    print("CUDA not available, using CPU for inference")
 
 DEVICE = "cuda:0" if torch.cuda.is_available() else "cpu"
 
@@ -27,10 +33,12 @@ model = InceptionResnetV1(
     num_classes=1,
     device=DEVICE
 )
-checkpoint = torch.load("weights\\resnetinceptionv1_epoch_32.pth",map_location=torch.device('cpu'))
+checkpoint = torch.load(
+    "weights\\resnetinceptionv1_epoch_32.pth", map_location=torch.device('cpu'))
 model.load_state_dict(checkpoint["model_state_dict"])
 model.to(DEVICE)
 model.eval()
+
 
 def predict(frame):
     """Predict the label of the input frame"""
@@ -46,20 +54,23 @@ def predict(frame):
         face_region = frame[y:y + h, x:x + w]  # Extract face region
 
         # Perform face recognition on the extracted face region
-        input_face = Image.fromarray(cv2.cvtColor(face_region, cv2.COLOR_BGR2RGB))
+        input_face = Image.fromarray(
+            cv2.cvtColor(face_region, cv2.COLOR_BGR2RGB))
         input_face = mtcnn(input_face)
         if input_face is None:
             continue
 
         input_face = input_face.unsqueeze(0)  # add the batch dimension
-        input_face = F.interpolate(input_face, size=(256, 256), mode="bilinear", align_corners=False)
+        input_face = F.interpolate(input_face, size=(
+            256, 256), mode="bilinear", align_corners=False)
         input_face = input_face.to(DEVICE).to(torch.float32) / 255.0
 
         target_layers = [model.block8.branch1[-1]]
         cam = GradCAM(model=model, target_layers=target_layers)
         targets = [ClassifierOutputTarget(0)]
 
-        grayscale_cam = cam(input_tensor=input_face, targets=targets, eigen_smooth=True)
+        grayscale_cam = cam(input_tensor=input_face,
+                            targets=targets, eigen_smooth=True)
         grayscale_cam = grayscale_cam[0, :]
         visualization = show_cam_on_image(
             input_face.squeeze(0).permute(1, 2, 0).cpu().detach().numpy(), grayscale_cam, use_rgb=True
@@ -72,13 +83,15 @@ def predict(frame):
 
         if prediction == "Fake":
             print("Deepfake detected, confidence: ", confidence * 100)
-            frame = cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 4)
+            frame = cv2.rectangle(
+                frame, (x, y), (x + w, y + h), (0, 0, 255), 4)
             frame = cv2.putText(frame, "Deep Fake Detected", (x, y - 10),
                                 cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 0, 255), 2)
             is_deepfake = True
         else:
             print("Real face, confidence: ", confidence * 100)
-            frame = cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 4)
+            frame = cv2.rectangle(
+                frame, (x, y), (x + w, y + h), (255, 0, 0), 4)
             frame = cv2.putText(frame, "Real Face", (x, y - 10),
                                 cv2.FONT_HERSHEY_SIMPLEX, 0.9, (255, 0, 0), 2)
             is_deepfake = False
--- df_server/main.py
+++ df_server/main.py
@@ -4,6 +4,7 @@ import numpy as np
 from face_detection import detect_bounding_box
 from deepfake_detection import predict
 from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
 
 app = FastAPI()
 app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_methods=["*"], allow_headers=["*"])
@@ -31,3 +32,6 @@ async def detect(file: UploadFile = File(...)):
         "confidence": round(confidence, 3),
         "message": "Detection complete"
     }
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
